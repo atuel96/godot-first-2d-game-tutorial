ded0a1bcfe0a310660f14f9c6d69a34b6313bdc3
Atuel <70816619+atuel96@users.noreply.github.com>
2024-02-24 02:23:58 -0300

    Add HUD

--- Main.gd
+++ Main.gd
@@ -5,7 +5,8 @@ var score : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
+	#new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,15 +17,21 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
